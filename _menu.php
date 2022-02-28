feb20b007b81080e3db31d368b3c9a80cf98a0e3
j4rl <charlie.jarl@gmail.com>
2022-02-28 14:16:16 +0100

    menu

--- _menu.php
+++ _menu.php
@@ -2,5 +2,4 @@
     <a href="index.php">Home</a>
     <?php if(isLevel(50)){ ?><a href="listuser.php">List user</a><?php } ?>
     <?php if(isLevel(80)){ ?><a href="adduser.php">Add user</a><?php } ?>
-    
 </nav>
